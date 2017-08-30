36096d893c40c2b7073f0ebe6366c3287f1f306b
NiuGenen <602131568@qq.com>
2017-08-30 20:04:30 +0800

    adjust .h

--- C13_RBTree/rb_tree.h
+++ C13_RBTree/rb_tree.h
@@ -49,6 +49,10 @@ void rb_tree_free(
     struct rb_tree** T,
     int if_free_child );
 
+/*
+ * core func
+ */
+
 int rb_insert(
     struct rb_tree* T,
     struct rb_node* node );
@@ -58,21 +62,67 @@ int rb_delete(
 struct rb_node* rb_find_node(
     struct rb_tree* T,
     RB_NODE_KEY_TYPE key );
+void rb_node_left_rotate(
+    struct rb_tree* T,
+    struct rb_node* node);
+void rb_node_right_rotate(
+    struct rb_tree* T,
+    struct rb_node* node);
+void rb_insert_fixup(
+    struct rb_tree* T,
+    struct rb_node* node);
+void rb_transplant(
+    struct rb_tree* T,
+    struct rb_node* old_,
+    struct rb_node* new_);
+void rb_delete_fixup(
+    struct rb_tree* T,
+    struct rb_node* fix);
 
-void rb_tree_pr( struct rb_tree* T );
-void rb_node_pr( struct rb_node* node, int if_pr_child );
-void rb_key_pr( RB_NODE_KEY_TYPE key );
+/*
+ * print func 
+ */
+
+void rb_tree_pr(  struct rb_tree* T );
+void rb_node_pr(  struct rb_node* node, int if_pr_child );
 void rb_color_pr( struct rb_node* node );
+void rb_key_pr(   RB_NODE_KEY_TYPE key );
 
-int rb_node_is_leaf( struct rb_node* node );
-int rb_node_has_left( struct rb_node* node );
-int rb_node_has_right( struct rb_node* node );
-int rb_node_has_parent( struct rb_node* node );
-int rb_node_is_black( struct rb_node* node );
-int rb_node_is_red( struct rb_node* node );
+/*
+ * common func 
+ */
+
+int rn_node_parent_red(   struct rb_node* node );
+int rn_node_parent_black( struct rb_node* node );
+
+int rb_node_is_root( struct rb_tree* T, struct rb_node* node );
+int rb_node_is_leaf(   struct rb_node* node );
+int rb_node_is_left(   struct rb_node* node );
+int rb_node_is_right(  struct rb_node* node );
+int rb_node_is_black(  struct rb_node* node );
+int rb_node_is_red(    struct rb_node* node );
+int rb_node_parent_id_left(  struct rb_node* node );
+int rb_node_parent_id_right( struct rb_node* node );
+
+int rb_node_has_left(         struct rb_node* node );
+int rb_node_has_right(        struct rb_node* node );
+int rb_node_has_parent(       struct rb_node* node );
+int rb_node_has_grand_parent( struct rb_node* node );
+
+/*
+ * check rb_tree 
+ */
 int rb_node_check_color(
     struct rb_node* node,
     int if_check_child );
 int rb_tree_check( struct rb_tree* T );
+int rb_tree_check_black_hight_get_hight(
+    struct rb_tree* T,
+    struct rb_node* node_leaf);
+int rb_tree_check_black_hight_help(
+    struct rb_tree* T,
+    struct rb_node* node );
+int rb_tree_check_black_hight(
+    struct rb_tree* T);
 
 #endif
